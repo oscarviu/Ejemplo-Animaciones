f8f6b51386fce8fd2ea3dd459a5579c4113b783f
oscarviu <oscar.sanfelix@campusviu.es>
2017-11-29 22:16:20 +0100

    Ejemplos de ejecución de animación sobre modelos animados y de creación de animaciones simples sobre objetos.
    Signed-off-by: oscarviu <oscar.sanfelix@campusviu.es>

---
+++ Assets/Scripts/Orc.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orc : MonoBehaviour {
+	BoxCollider box;
+	Animator anim = null;
+	float original_z = 0;
+
+	// Use this for initialization
+	void Start () {
+		box = GetComponent<BoxCollider>();
+		original_z = box.center.z;
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		box.center = new Vector3(box.center.x, box.center.y, anim.GetFloat("AttackDistance") * original_z * 6);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	static int speedHash = Animator.StringToHash("Speed");
+	static int turnHash = Animator.StringToHash("Turn");
+	static int crouchHash = Animator.StringToHash("Crouch");
+	static int jumpHash = Animator.StringToHash("Jump");
+
+	Animator anim;
+	float h = 0;
+	float v = 0;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Agacharse
+		if(Input.GetKey(KeyCode.C))
+			anim.SetBool(crouchHash, true);
+		else
+			anim.SetBool(crouchHash, false);
+
+
+		// Saltar
+		if (Input.GetKeyDown(KeyCode.Space))
+			anim.SetTrigger(jumpHash);
+
+
+		// Girar, caminar y correr
+		float axisHorizontal = Input.GetAxis("Horizontal");
+		h = Mathf.Lerp(h, axisHorizontal, Time.deltaTime * 10);
+		v = Input.GetKey(KeyCode.LeftShift) ? Input.GetAxis("Vertical") * 2 : Input.GetAxis("Vertical");
+
+		anim.SetFloat(speedHash, v);
+		anim.SetFloat(turnHash, h);
+	}
+}
